214ae3450fa3de38b28cbd2f514051d2e673fb07
Akshatsahay18 <163159541+Akshatsahay18@users.noreply.github.com>
2025-09-14 04:51:16 +0530

    Update constants.js

--- 2d-portfolio-kaboom/src/constants.js
+++ 2d-portfolio-kaboom/src/constants.js
@@ -1,10 +1,10 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  pc: `This is my PC. I work mostly in C++,Python these days.
+    I also like Javascript . Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Akshatsahay18" target="_blank">Github</a>!`,
+  "cs-degree": `This is my ECE degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
   You'll learn how I built this portfolio you're currently playing through!`,
@@ -21,3 +21,4 @@ export const dialogueData = {
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
 };
+
