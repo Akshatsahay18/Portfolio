097c154ae095894aa19d61d328205a84e923b75f
Akshatsahay18 <163159541+Akshatsahay18@users.noreply.github.com>
2025-09-17 00:07:45 +0530

    Update constants.js

--- 2d-portfolio-kaboom/src/constants.js
+++ 2d-portfolio-kaboom/src/constants.js
@@ -7,16 +7,16 @@ export const dialogueData = {
   "cs-degree": `This is my ECE degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube . 
  i also do progamming here i also have keen intrest in game development,ML and web developmentt,
-  tv: `That's my TV. I've been watching many tech channel lately as i like to expand my horizons in every field'  : people say jack of all trades is master of none but they forget its ofen beeter than master of one
+  tv: `That's my TV. I've been watching many tech channels lately because I like to expand my horizons in every field'  : people say jack of all trades is master of none but they forget its ofen better than master of one
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1835vl1LBuDhktYbT7V67AfNm4jMvTymW/view" target="_blank">Check it out?</a>
   Contact me at akshatsahay123@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
   library: `There are a lot of programming books on my shelves. There is even one in English (I also speak English btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit Akshat's portfolio, just close the tab.`,
 };
+
 
 
