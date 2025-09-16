e34701012ae203485842628c141a8602e4e2028c
Akshatsahay18 <163159541+Akshatsahay18@users.noreply.github.com>
2025-09-17 00:06:23 +0530

    Update constants.js

--- 2d-portfolio-kaboom/src/constants.js
+++ 2d-portfolio-kaboom/src/constants.js
@@ -5,20 +5,18 @@ export const dialogueData = {
     I also like Javascript . Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/Akshatsahay18" target="_blank">Github</a>!`,
   "cs-degree": `This is my ECE degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube . 
+ i also do progamming here i also have keen intrest in game development,ML and web developmentt,
+  tv: `That's my TV. I've been watching many tech channel lately as i like to expand my horizons in every field'  : people say jack of all trades is master of none but they forget its ofen beeter than master of one
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1835vl1LBuDhktYbT7V67AfNm4jMvTymW/view" target="_blank">Check it out?</a>
+  Contact me at akshatsahay123@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
   Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  library: `There are a lot of programming books on my shelves. There is even one in English (I also speak English btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit Akshat's portfolio, just close the tab.`,
 };
+
 
